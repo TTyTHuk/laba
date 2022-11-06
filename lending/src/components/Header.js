451d30aca68f02ba49b66171ec50c36043a86b65
TTyTHuk <1belotserkovec1@gmail.com>
2022-11-06 14:29:27 +0300

    push

---
+++ lending/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css"
+export default function header(){
+    
+    return (
+        <div className="head">
+
+            <h2>
+            Интернет технологии          
+            </h2>
+            <h2>
+            Белоцерковец Александр ИДМ-22-06
+            </h2>
+        </div>
+    )
+
+}
